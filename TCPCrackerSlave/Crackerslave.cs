5be40d96fd388a4802ae54ceb6332ab29f8fdfa1
jakobln <62882745+jakobln@users.noreply.github.com>
2022-06-15 13:35:20 +0200

    Add project files.

---
+++ TCPCrackerSlave/Crackerslave.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PasswordCrackerCentralized;
+
+namespace TCPCrackerSlave
+{
+    class Crackerslave
+    {
+        private List<string> wordlist = new();
+
+        public Crackerslave()
+        {
+
+        }
+
+
+        internal void connect(string host, int port)
+        {
+            TcpClient clientSocket = new TcpClient(host, port);
+
+            Stream ns = clientSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+
+           sw.AutoFlush = true;
+           Console.ReadLine();
+            sw.WriteLine("chunk");
+           
+
+            var word = sr.ReadLine();
+            
+            wordlist = JsonSerializer.Deserialize<List<string>>(word);
+            foreach (var x in wordlist)
+            {
+                Console.WriteLine(x);
+            }
+            
+
+
+
+            DoCrack(wordlist);
+    
+             
+
+            Console.ReadKey();
+            ns.Close();
+            clientSocket.Close();
+            Console.ReadKey();
+        }
+
+        private void DoCrack(List<string> list)
+        {
+            Cracking crack = new Cracking();
+            crack.RunCracking();
+        }
+    }
+}
---
+++ TCPCrackerSlave/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+
+namespace TCPCrackerSlave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cracker slave");
+            Crackerslave cs = new Crackerslave();
+            cs.connect("192.168.14.112", 7777);
+
+        }
+    }
+}
